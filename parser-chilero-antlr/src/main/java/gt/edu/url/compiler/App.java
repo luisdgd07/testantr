8b9baf5de94f3b75eb7388f5ea2fd7b4cff40e91
Angie Schumann <98908820+Angie-Schumann@users.noreply.github.com>
2023-11-15 03:01:27 -0500

    ANTLR

--- parser-chilero-antlr/src/main/java/gt/edu/url/compiler/App.java
+++ parser-chilero-antlr/src/main/java/gt/edu/url/compiler/App.java
@@ -1,42 +1,52 @@
 package gt.edu.url.compiler;
 
+import java_cup.runtime.Symbol;
 
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.InputStreamReader;
 
-import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
-import java_cup.runtime.Symbol;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        args = Flags.handleFlags(args);
+		for (String string : args) {
+			try {
 
-		FileReader file = null;
-	try {
-		String inputFile = args[0];
-		FileInputStream inputStream = new FileInputStream(inputFile);
-		ANTLRInputStream input = new ANTLRInputStream(inputStream);
-	    CoolLexer lexer = new CoolLexer(input);
-		CommonToken token;
-		while ((token= (CommonToken) lexer.nextToken()).getType()!= TokenConstants.EOF) {
-			Utilities.dumpToken(System.out, lexer.getLine(), token);
-		}
-		CommonTokenStream tokens = new CommonTokenStream(lexer);
-		CoolParser parser = new CoolParser(tokens);
-		ParseTree arbol = parser.sintactico();
+				CoolLexer lexer = new CoolLexer(CharStreams.fromFileName(string));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        CoolParser parser = new CoolParser(tokens);
+        ParseTree tree = parser.sintactico();
+
 		CoolVisitor_ cool = new CoolVisitor_();
-		cool.visit(arbol);
+		cool.visit(tree);
 
-		System.out.println(cool.visit(arbol).toString());
-	} catch (Exception ex) {
-	    ex.printStackTrace(System.err);
-	}
+		System.out.println(cool.visit(tree).toString());
+/*
+            CoolTokenLexer lexer = new CoolTokenLexer(new InputStreamReader(new FileInputStream(string)));
+            CoolParser parser = new CoolParser(lexer);
+            Symbol result = (Flags.parser_debug
+                    ? parser.debug_parse()
+                    : parser.parse());
+            if (parser.omerrs > 0) {
+                System.err.println("Compilation halted due to lex and parse errors");
+                System.exit(1);
+            }
+            ((Program)result.value).dump_with_types(System.out, 0);
+			 */
+        } catch (Exception ex) {
+            ex.printStackTrace(System.err);
+            Utilities.fatalError("Unexpected exception in parser");
+        }
+		}
+        
     }
 
 }
